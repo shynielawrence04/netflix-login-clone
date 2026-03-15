8aa5e5cd8455b9dbb4fc4c36257d5e5e6904f6de
shynielawrence04 <shynielawrence02@gmail.com>
2026-03-15 12:22:16 +0200

    fix render port

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const cors = require('cors')
 
 const app = express()
-const PORT = 5000
+const PORT = process.env.PORT || 5000
 
 app.use(cors())
 app.use(express.json())
@@ -43,5 +43,5 @@ app.post('/login', (req, res) => {
 })
 
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`)
+  console.log(`Server running on port ${PORT}`)
 })
